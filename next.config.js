16d835fc2ed104aacb69f2b5e81b4ec6425ceb2f
tarunsai217 <81076055+tarunsai217@users.noreply.github.com>
2023-11-24 13:41:38 +0530

    navigation added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bravaland.com",
+        port: "",
+        pathname:
+          "https://bravaland.com/cdn/shop/files/Artboard_1.png?v=1678787853",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: ["bravaland.com"],
+  },
+};
